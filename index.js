5f02d46d88bec6dee283b87a29322392e13b6c84
ALPHA ALVIN <alpha.muchemi@student.moringaschool.com>
2024-07-29 19:54:03 +0300

    tasklister

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const taskForm = document.getElementById('create-task-form');
+  
+    taskForm.addEventListener('submit', function(event) {
+      event.preventDefault(); 
+
+    });
+  });
+  
+  
